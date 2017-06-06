e7d7f91d5dafcaa0fb3c6370192750d2a99688ba
artem <------>
2017-06-06 23:37:56 +0300

    ~

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => '',
-    'password' => '',
+    'username' => 'root',
+    'password' => 'al2216w',
     'charset' => 'utf8',
 ];
