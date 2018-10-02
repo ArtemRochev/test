9e24f0d4b2b8f3fd19729beea7825a21066a6c5e
artem3 <rochevartem@tireconnect.ca>
2018-10-02 17:00:18 +0300

    test

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => 'root',
-    'password' => 'al2216w',
-    'charset' => 'utf8',
-];
